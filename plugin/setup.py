1a97fc0bb71a9b0a145267f2c1f6fd2abf5a7eb9
Developer-Geekay <developergeekay@gmail.com>
2026-07-30 08:17:15 +0300

    fix: bump version to 1.0.1 and make release zip resolution dynamic

--- plugin/setup.py
+++ plugin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hostpanel-storage",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=["fastapi", "pydantic"],
     entry_points={
